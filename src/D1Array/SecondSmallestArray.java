49df90f4876d3d5ba1c9d5353358442ef0c40eae
Amit Yadav <ay3106222@gmail.com>
2025-09-24 19:34:58 +0530

    find the second smallest value in array and write for programme for find to array

--- src/D1Array/SecondSmallestArray.java
+++ src/D1Array/SecondSmallestArray.java
@@ -1,5 +1,21 @@
 package D1Array;
 
 public class SecondSmallestArray {
+    public static void main(String[] args) {
+        //create array
+        int[] arr={2,3,5,6,8,9,7};
 
+        int first=Integer.MAX_VALUE;
+        int second=Integer.MAX_VALUE;
+
+        for (int val:arr){
+            if(val<first){
+                second=first;
+                first=val;
+            } else if (val<second && val!=first) {
+                second=val;
+            }
+        }
+        System.out.println("Second Smallest value: "+second);
+    }
 }
