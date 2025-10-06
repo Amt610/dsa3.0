2e3089d484038f80ea769d50a41050c90b6fe440
Amit Yadav <ay3106222@gmail.com>
2025-10-06 22:10:39 +0530

    code for Inheritance for MultilevelInheritance.java update some code

--- src/InheritanceOOPs/MultilevelInheritance.java
+++ src/InheritanceOOPs/MultilevelInheritance.java
@@ -16,9 +16,17 @@ class Monitor extends Cpu{
 
 
 //Create another class
-
+class KeyBoard extends Monitor{
+    void KeyBoardDetails(){
+        System.out.println("This is Computer Controller Parts...");
+    }
+}
 public class MultilevelInheritance {
     public static void main(String[] args) {
-
+        //Create Object of Last Child
+        KeyBoard kb=new KeyBoard();
+        kb.CpuDetails();
+        kb.KeyBoardDetails();
+        kb.MonitorDetails();
     }
 }
