7ac64632f96a7f44e075aae46ca6f6115613b219
Amit Yadav <ay3106222@gmail.com>
2025-10-06 22:07:22 +0530

    code for Inheritance for MultilevelInheritance.java update some code

--- src/InheritanceOOPs/MultilevelInheritance.java
+++ src/InheritanceOOPs/MultilevelInheritance.java
@@ -1,5 +1,22 @@
 package InheritanceOOPs;
 
+//Create java Class MultilevelInheritance
+class Cpu{
+    void CpuDetails(){
+        System.out.println("CPU is Computer Mind..");
+    }
+}
+
+//create another class to name monitor
+class Monitor extends Cpu{
+    void MonitorDetails(){
+        System.out.println("Monitor is Most Important Part of Computer...");
+    }
+}
+
+
+//Create another class
+
 public class MultilevelInheritance {
     public static void main(String[] args) {
 
